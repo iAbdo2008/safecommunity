7da21dab925aebdcdd64d7a347c29ab5c049a963
iAbdo2008 <82214170+iAbdo2008@users.noreply.github.com>
2021-04-21 18:05:29 +0200

    Update settings.py

--- socialnetwork/settings.py
+++ socialnetwork/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5&8=#n81c#1p5*10munpusdse))br(j*&5@4zgm*1$zet4u4tj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', 'safechat.ddns.net', '5a9ab94b929c.ngrok.io', '127.0.0.1', 'safecommunitys.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'safecommunitys.herokuapp.com']
 
 
 # Application definition
