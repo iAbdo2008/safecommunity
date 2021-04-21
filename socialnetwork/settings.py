ea870f0a3778458dffab1c6512ecec3cd6a89e7f
iAbdo2008 <82214170+iAbdo2008@users.noreply.github.com>
2021-04-21 17:37:54 +0200

    Update settings.py

--- socialnetwork/settings.py
+++ socialnetwork/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 CRISPY_TEMPLATE_PACK = "bootstrap4"
