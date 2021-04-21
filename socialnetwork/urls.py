6510e3e07ab2e4391446f6b2e65a43f62c45842a
iAbdo2008 <82214170+iAbdo2008@users.noreply.github.com>
2021-04-21 18:15:52 +0200

    Update urls.py

--- socialnetwork/urls.py
+++ socialnetwork/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('social/', include('social.urls'))
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
 ]
