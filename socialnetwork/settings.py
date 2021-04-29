0c68f666143c255c816ec56c2186b842f3ddc9ee
iAbdo2008 <82214170+iAbdo2008@users.noreply.github.com>
2021-04-29 23:30:36 +0200

    Update settings.py

--- socialnetwork/settings.py
+++ socialnetwork/settings.py
@@ -94,11 +94,15 @@ WSGI_APPLICATION = 'socialnetwork.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+DATABASES = {  
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'djangoApp',  
+        'USER':'root',  
+        'PASSWORD':'mysql',  
+        'HOST':'ec2-3-217-219-146.compute-1.amazonaws.com',  
+        'PORT':'5432'  
+    }  
 }
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
