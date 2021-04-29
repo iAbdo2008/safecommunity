bf74652cd1d7e8b98bded5090b7f9cf11c232f6a
iAbdo2008 <82214170+iAbdo2008@users.noreply.github.com>
2021-04-29 15:19:31 +0200

    Update views.py

--- social/views.py
+++ social/views.py
@@ -253,7 +253,10 @@ class Love(LoginRequiredMixin, View):
 	
 class FPostListView(LoginRequiredMixin, View):
 	def get(self, request, *args, **kwargs):
-		posts = Post.objects.all().order_by('-created_on')
+		        logged_in_user = request.user
+        posts = Post.objects.filter(
+            author__profile__followers__in=[logged_in_user.id]
+        ).order_by('-created_on')
 		form = PostForm()
 
 		context = {
