49bc0ecb2cf978867dceb5551a57c6baa8c12bb9
iAbdo2008 <82214170+iAbdo2008@users.noreply.github.com>
2021-05-08 14:52:36 +0200

    Update models.py

--- social/models.py
+++ social/models.py
@@ -17,7 +17,7 @@ class Comment(models.Model):
 	author = author = models.ForeignKey(User, on_delete=models.CASCADE)
 	post = models.ForeignKey('Post', on_delete=models.CASCADE)
 	likes = models.ManyToManyField(User, blank=True, related_name='comment_likes')
-	dislikes = modles.ManyToManyField(User, blank=True, related_name='comment_dislikes')
+	dislikes = models.ManyToManyField(User, blank=True, related_name='comment_dislikes')
 
 class UserProfile(models.Model):
 	user = models.OneToOneField(User, primary_key=True, verbose_name='user', related_name='profile', on_delete=models.CASCADE)
