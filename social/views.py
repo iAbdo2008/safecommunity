2faa546b2c4e9ebf085f719ea011882ce931862f
iAbdo2008 <82214170+iAbdo2008@users.noreply.github.com>
2021-05-14 12:19:04 +0200

    Update views.py

--- social/views.py
+++ social/views.py
@@ -340,3 +340,83 @@ class FCommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 	def test_func(self):
 		post = self.get_object()
 		return self.request.user == post.author
+
+class GroupListView(LoginRequiredMixin, View):
+	def get(self, request, *args, **kwargs):
+		posts = Post.objects.all().order_by('-created_on')
+		form = PostForm()
+
+		context = {
+			'post_list': posts,
+			'form': form,
+		}
+
+		return render(request, 'Social/post_list.html', context)
+	def post(self, request, *args, **kwargs):
+		posts = Post.objects.all().order_by('-created_on')
+		form = form = PostForm(request.POST, request.FILES) 
+
+		if form.is_valid():
+			new_post = form.save(commit=False)
+			new_post.author = request.user
+			new_post.save()
+
+		context = {
+			'post_list': posts,
+			'form': form,
+		 } 
+
+		return render(request, 'Social/post_list.html', context)
+
+class PostDetailView(LoginRequiredMixin, View):
+	def get(self, request, pk, *args, **kwargs):
+		post = Post.objects.get(pk=pk)
+		form  = CommentForm()
+		comments =  Comment.objects.filter(post=post).order_by('-created_on')
+		context = {
+
+			'post':  post,
+			'form':form,
+			'comments': comments,
+		}
+
+		return render(request, 'Social/post_detail.html', context)
+	def post(self, request, pk, *args, **kwargs):
+		post = Post.objects.get(pk=pk)
+		form  = CommentForm(request.POST)
+		if form.is_valid():
+			new_comment = form.save(commit=False)
+			new_comment.author = request.user
+			new_comment.post = post
+			new_comment.save()
+
+		comments =  Comment.objects.filter(post=post).order_by('-created_on')
+		context = {
+
+			'post':  post,
+			'form':form,
+			'comments': comments,
+		}
+
+		return render(request, 'Social/post_detail.html', context)
+
+class PostEditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+	model = Post
+	fields  = ['body','image']
+	template_name = 'Social/post_edit.html'
+
+	def get_success_url(self):
+		pk = self.kwargs['pk']
+		return reverse_lazy('post-detail', kwargs={'pk': pk})
+
+	def test_func(self):
+		post = self.get_object()
+		return self.request.user == post.author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+	model  = Post
+	template_name =  'Social/post_delete.html'
+	success_url = reverse_lazy('post-list')
+	def test_func(self):
+		post = self.get_object()
+		return self.request.user == post.author
