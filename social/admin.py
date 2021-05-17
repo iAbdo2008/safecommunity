ad58789aee5546927098004e8135f756de56401b
iAbdo2008 <82214170+iAbdo2008@users.noreply.github.com>
2021-05-17 22:43:21 +0200

    Update admin.py

--- social/admin.py
+++ social/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, UserProfile
+from .models import Post, UserProfile, Notification
 
 admin.site.register(Post)
 admin.site.register(UserProfile)
