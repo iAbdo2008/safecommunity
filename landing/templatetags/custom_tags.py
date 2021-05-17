b3ecc961095cf4a9a888dd149415f39e7f3d7c9e
iAbdo2008 <82214170+iAbdo2008@users.noreply.github.com>
2021-05-17 23:02:59 +0200

    Update custom_tags.py

--- landing/templatetags/custom_tags.py
+++ landing/templatetags/custom_tags.py
@@ -3,8 +3,8 @@ from social.models import Notification
 
 register = template.Library()
 
-@register.inclusion_tag('Social/show_notifications.html', takes_context=True)
+@register.inclusion_tag('social/show_notifications.html', takes_context=True)
 def show_notifications(context):
-    request_user = context['request'].user
-    notifications = Notification.objects.filter(to_user=request_user).exclude(user_has_seen=True).order_by('-date')
-    return {'notifications': notifications}
+	request_user = context['request'].user
+	notifications = Notification.objects.filter(to_user=request_user).exclude(user_has_seen=True).order_by('-date')
+	return {'notifications': notifications}
