9348e4ef11ceb8f4776aaf6a668d5e3973d4fcf1
iAbdo2008 <82214170+iAbdo2008@users.noreply.github.com>
2021-05-20 11:53:48 +0200

    Update settings.py

--- socialnetwork/settings.py
+++ socialnetwork/settings.py
@@ -144,7 +144,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATICFILES_DIRS[
+STATICFILES_DIRS = [
+    
     BASE_DIR/ 'static'
 ]
 STATIC_URL = '/static/'
