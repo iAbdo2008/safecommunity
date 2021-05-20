2b3f74dafb2aef91d46148a64b63f97e0c6b73a6
iAbdo2008 <82214170+iAbdo2008@users.noreply.github.com>
2021-05-20 12:00:13 +0200

    Update settings.py

--- socialnetwork/settings.py
+++ socialnetwork/settings.py
@@ -149,7 +149,7 @@ STATICFILES_DIRS = [
     
     BASE_DIR/ 'static'
 ]
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_ROOT = BASE_DIR / 'media'
 CLOUDINARY_STORAGE = {
