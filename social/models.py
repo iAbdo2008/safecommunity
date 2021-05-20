e4a1d4e1c15af0d85c07eb3721812dffd71f0965
iAbdo2008 <82214170+iAbdo2008@users.noreply.github.com>
2021-05-20 13:00:25 +0200

    Update models.py

--- social/models.py
+++ social/models.py
@@ -36,9 +36,9 @@ def save_user_profile(sender, instance, created, **kwargs):
 class Notification(models.Model):
 	# 1 = Like, 2 = Comment, 3 = Follow
 	notification_type = models.IntegerField()
-	to_user = models.ForeignKey(User, related_name='notification_to', on_delete=models.CASCADE, blank=True, null=True)
-	from_user = models.ForeignKey(User, related_name='notification_from', on_delete=models.CASCADE, blank=True, null=True)
-	post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='+', null=True)
-	comment = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='+', null=True)
+	to_user = models.ForeignKey(User, related_name='notification_to', on_delete=models.CASCADE, null=True)
+	from_user = models.ForeignKey(User, related_name='notification_from', on_delete=models.CASCADE, null=True)
+	post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='+', blank=True, null=True)
+	comment = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='+', blank=True, null=True)
 	date = models.DateTimeField(default=timezone.now)
 	user_has_seen = models.BooleanField(default=False)
