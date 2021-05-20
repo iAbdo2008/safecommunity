7089af12cadece693501496c1c1c87d017185745
iAbdo2008 <82214170+iAbdo2008@users.noreply.github.com>
2021-05-20 12:57:55 +0200

    Update models.py

--- social/models.py
+++ social/models.py
@@ -36,8 +36,8 @@ def save_user_profile(sender, instance, created, **kwargs):
 class Notification(models.Model):
 	# 1 = Like, 2 = Comment, 3 = Follow
 	notification_type = models.IntegerField()
-	to_user = models.ForeignKey(User, related_name='notification_to', on_delete=models.CASCADE, null=True)
-	from_user = models.ForeignKey(User, related_name='notification_from', on_delete=models.CASCADE, null=True)
+	to_user = models.ForeignKey(User, related_name='notification_to', on_delete=models.CASCADE, blank=True, null=True)
+	from_user = models.ForeignKey(User, related_name='notification_from', on_delete=models.CASCADE, blank=True, null=True)
 	post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='+', null=True)
 	comment = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='+', null=True)
 	date = models.DateTimeField(default=timezone.now)
