adee5b92156ecc4cceb71427bf3d787b3ec67ed2
iAbdo2008 <82214170+iAbdo2008@users.noreply.github.com>
2021-05-20 12:41:21 +0200

    Update models.py

--- social/models.py
+++ social/models.py
@@ -6,7 +6,7 @@ from django.dispatch import receiver
 
 class Post(models.Model):
 	body = models.TextField()
-	#image = models.ImageField(upload_to='media/uploads/post_photos', blank=True, null=True) 
+	image = models.ImageField(upload_to='media/uploads/post_photos', blank=True, null=True) 
 	created_on = models.DateTimeField(default=timezone.now)
 	author = models.ForeignKey(User, on_delete=models.CASCADE)
 	likes = models.ManyToManyField(User, blank=True, related_name='likes')
@@ -24,7 +24,7 @@ class UserProfile(models.Model):
 	bio = models.TextField(max_length=500, blank=True, null=True)
 	birth_date = models.DateField(null=True, blank=True)
 	location = models.CharField(max_length=100, blank=True, null=True)
-	#picture = models.ImageField(upload_to="media/uploads/profile_pictures/", default="media/uploads/profile_pictures/default_wahrls.png")
+	picture = models.ImageField(upload_to="media/uploads/profile_pictures/", default="media/uploads/profile_pictures/default_wahrls.png")
 	followers = models.ManyToManyField(User, blank=True, related_name='followers')
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
