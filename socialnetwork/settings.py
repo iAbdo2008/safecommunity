3a3e26b3b09a6409c4753e65696a3f1f64bb35b7
iAbdo2008 <82214170+iAbdo2008@users.noreply.github.com>
2021-05-20 11:58:07 +0200

    Update settings.py

--- socialnetwork/settings.py
+++ socialnetwork/settings.py
@@ -144,12 +144,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     
     BASE_DIR/ 'static'
 ]
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_ROOT = BASE_DIR / 'media'
 CLOUDINARY_STORAGE = {
